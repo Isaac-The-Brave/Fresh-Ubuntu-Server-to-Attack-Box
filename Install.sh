d1f2be432e656ab502fd53ab0f521359ea11eb86
Isaac The Brave <53839630+Isaac-The-Brave@users.noreply.github.com>
2021-03-27 15:49:52 +0100

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,327 +1,121 @@
 #!/bin/bash
 
-GREEN='\033[0;32m'
-NC='\033[0m'
+# This script is supposed to help install all the necessary tools to run an automated scan of a domain.
 
-echo -e "${GREEN}[*] Bug Bounty Toolkit Installer${NC}"
-echo -e "${GREEN}[*] Setting Up Directories${NC}"
+echo "updating and upgrading"
 
-cd $HOME
-mkdir toolkit
-mkdir ~/toolkit/wordlists
+sudo apt-get update
+sudo apt-get upgrade
 
-echo -e "${GREEN}[*] Installing Essentials${NC}"
-apt-get update
-apt-get install -y build-essential
-apt-get install -y gcc 
-apt-get install -y git
-apt-get install -y vim 
-apt-get install -y wget 
-apt-get install -y curl
-apt-get install -y awscli
-apt-get install -y inetutils-ping 
-apt-get install -y make 
-apt-get install -y nmap 
-apt-get install -y whois 
-apt-get install -y python3
-apt-get install -y python-pip 
-apt-get install -y perl 
-apt-get install -y nikto
-apt-get install -y dnsutils 
-apt-get install -y net-tools
-apt-get install -y zsh
-apt-get install -y nano
-apt-get install -y tmux
-echo -e "${GREEN}[*] Essentials installed${NC}"
+clear
+# Install necessary official packages
+echo "installing necessary official packages"
 
+sudo apt-get install -y libcurl4-openssl-dev
+sudo apt-get install -y libssl-dev
+sudo apt-get install -y jq
+sudo apt-get install -y ruby-full
+sudo apt-get install -y libcurl4-openssl-dev libxml2 libxml2-dev libxslt1-dev ruby-dev build-essential libgmp-dev zlib1g-dev
+sudo apt-get install -y build-essential libssl-dev libffi-dev python-dev
+sudo apt-get install -y python-setuptools
+sudo apt-get install -y libldns-dev
+sudo apt-get install -y python3-pip
+sudo apt-get install -y python-pip
+sudo apt-get install -y python-dnspython
+sudo apt-get install -y git
+sudo apt-get install -y rename
+sudo apt-get install -y xargs
+sudo apt-get install -y golang
+sudo apt-get install -y python-dnspython
 
-#install go
-sudo snap install go
+clear
 
-# Install Tomnomnomnom's best tools
+# Ready directories for downloads
 
-cd ~/toolkit
-echo "done"
+echo "Making Directories for downloads"
 
+mkdir ~/BugBounty
+mkdir ~/BugBounty/Tools
+
+# Start downloading and installin tools
+
+# ffuf - https://github.com/ffuf/ffuf
+echo "installing ffuf" 
+
+go get -u github.com/ffuf/ffuf
+
+echo "ffuf installed"
+
+# httprobe -https://github.com/tomnomnom/httprobe
 echo "installing httprobe"
-go get -u github.com/tomnomnom/httprobe 
-echo "done"
 
-echo "installing unfurl"
-go get -u github.com/tomnomnom/unfurl 
-echo "done"
+go get -u github.com/tomnomnom/httprobe
 
-echo "installing waybackurls"
-go get -u github.com/tomnomnom/waybackurls
-echo "done"
+echo "httprobe installed"
 
-echo "installing ffuf"
-sudo go get -u github.com/ffuf/ffuf 
-echo "done"
-
-# Install Gospider
+# Gospider - https://github.com/jaeles-project/gospider
 echo "installing Gospider"
+
 go get -u github.com/jaeles-project/gospider
-echo "done"
 
-# amass
-echo -e "${GREEN}[*] Installing amass${NC}"
-export GO111MODULE=on
-go get -v github.com/OWASP/Amass/v3/...
+echo "Gospider installed"
 
-# subfinder
-echo -e "${GREEN}[*] Installing subfinder${NC}"
-go get -v github.com/projectdiscovery/subfinder/cmd/subfinder
+# Waybackurls - https://github.com/tomnomnom/waybackurls
+echo "installing waybackurls"
 
-#install aquatone
-echo "Installing Aquatone"
-go get github.com/michenriksen/aquatone
-echo "done"
+go get -u github.com/tomnomnom/waybackurls
 
-# Move 'go' binaries
-echo "Moving go binaries"
+echo "waybackurls installed"
+# Nuclei - https://github.com/projectdiscovery/nuclei
+echo "installing nuclei"
+
+GO111MODULE=on go get -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei
+
+echo "Nuclei installed"
+
+# Qsreplace - https://github.com/tomnomnom/qsreplace
+echo "installing qsreplace"
+
+go get -u github.com/tomnomnom/qsreplace
+
+echo "qsreplace installed"
+
+# kxss - https://github.com/tomnomnom/hacks/tree/master/kxss
+echo "installing kxss"
+
 cd ~/go/bin/
-mv * /bin/
-echo "done"
+wget https://raw.githubusercontent.com/tomnomnom/hacks/master/kxss/main.go
+go build main.go
+rm main.go
+mv main kxss
 
-# Nmap
-echo -e "${GREEN}[*] Installing Nmap${NC}"
-apt-get install -y nmap
+echo "kxss installed"
 
-# masscan
-echo -e "${GREEN}[*] Installing masscan${NC}"
-cd ~/toolkit
-apt-get install -y libpcap-dev
-git clone https://github.com/robertdavidgraham/masscan.git
-cd masscan
-make
-ln -sf ~/toolkit/masscan/bin/masscan /bin/masscan    
+# dsss - https://github.com/stamparm/DSSS​
+echo "installing DSSS"
 
-# dnsenum
-echo -e "${GREEN}[*] Installing dnsenum${NC}"
-apt-get install -y cpanminus 
-cd ~/toolkit 
-git clone https://github.com/fwaeytens/dnsenum.git 
-cd dnsenum/ 
-chmod +x dnsenum.pl 
-ln -s ~/toolkit/dnsenum/dnsenum.pl /bin/dnsenum 
-cpanm String::Random 
-cpanm Net::IP 
-cpanm Net::DNS 
-cpanm Net::Netmask
-cpanm XML::Writer
+cd ~/BugBounty/Tools/
+git clone https://github.com/stamparm/DSSS
 
-# massdns
-echo -e "${GREEN}[*] Installing massdns${NC}"
-apt-get install -y libldns-dev
-cd ~/toolkit 
-git clone https://github.com/blechschmidt/massdns.git
-cd massdns/
-make
-ln -sf ~/toolkit/massdns/bin/massdns /bin/massdns
+cd ~ 
+echo "DSSS installed"
 
-# altdns
-echo -e "${GREEN}[*] Installing altdns${NC}"
-cd ~/toolkit 
-git clone https://github.com/infosec-au/altdns.git 
-cd altdns 
-pip install -r requirements.txt 
-chmod +x setup.py 
-python setup.py install
+# Sublist3r - https://github.com/aboul3la/Sublist3r
+echo "installing Sublist3r" 
+cd ~/BugBounty/Tools/
+git clone https://github.com/aboul3la/Sublist3r
+cd Sublist3r
+pip3 install -r requirements
 
-# thc-hydra
-echo -e "${GREEN}[*] Installing thc-hydra${NC}"
-apt-get install -y hydra
+cd ~
+# Dalfox - https://github.com/hahwul/dalfox​
+echo "Installing dalfox"
 
-# Sublist3r
-echo -e "${GREEN}[*] Installing Sublist3r${NC}"
-cd ~/toolkit 
-git clone https://github.com/aboul3la/Sublist3r.git
-cd Sublist3r/
-pip install -r requirements.txt
-ln -s ~/toolkit/Sublist3r/sublist3r.py /bin/sublist3r
+GO111MODULE=on go get -v github.com/hahwul/dalfox/v2
 
-# knock
-echo -e "${GREEN}[*] Installing Knockpy${NC}"
-apt-get install -y python-dnspython 
-cd ~/toolkit
-git clone https://github.com/guelfoweb/knock.git
-cd knock
-chmod +x setup.py
-python setup.py install
+echo "dalfox installed"
 
-# dirb
-echo -e "${GREEN}[*] Installing dirb${NC}"
-apt-get install -y dirb
+# Blind XSS - https://xsshunter.com
+# Requires registration (best to replace this with something more mobile)
 
-# teh_s3_bucketeers
-echo -e "${GREEN}[*] Installing teh_s3_bucketeers${NC}"
-cd ~/toolkit
-git clone https://github.com/tomdev/teh_s3_bucketeers.git 
-cd teh_s3_bucketeers 
-chmod +x bucketeer.sh 
-ln -sf ~/toolkit/teh_s3_bucketeers/bucketeer.sh /bin/bucketeer
-
-# Recon-ng
-echo -e "${GREEN}[*] Installing Recon-ng${NC}"
-cd ~/toolkit 
-git clone https://github.com/lanmaster53/recon-ng.git 
-cd recon-ng 
-apt-get install -y python3-pip 
-pip3 install -r REQUIREMENTS 
-chmod +x recon-ng 
-ln -sf ~/toolkit/recon-ng/recon-ng /bin/recon-ng
-
-# XSStrike
-echo -e "${GREEN}[*] Installing XSStrike${NC}"
-cd ~/toolkit
-git clone https://github.com/s0md3v/XSStrike.git 
-cd XSStrike 
-apt-get install -y python3-pip 
-pip3 install -r requirements.txt 
-chmod +x xsstrike.py
-ln -sf ~/toolkit/XSStrike/xsstrike.py /bin/xsstrike
-
-# sqlmap
-echo -e "${GREEN}[*] Installing sqlmap${NC}"
-apt-get install -y sqlmap
-
-# wfuzz
-echo -e "${GREEN}[*] Installing wfuzz${NC}"
-pip install --upgrade setuptools 
-apt-get install -y python-pycurl 
-pip install wfuzz
-
-# wafw00f
-echo -e "${GREEN}[*] Installing wafw00f${NC}"
-cd ~/toolkit 
-git clone https://github.com/enablesecurity/wafw00f.git 
-cd wafw00f 
-chmod +x setup.py 
-python setup.py install
-
-
-
-# wpscan
-echo -e "${GREEN}[*] Installing wpscan${NC}"
-apt-get install -y libcurl4-openssl-dev libxml2 libxml2-dev libxslt1-dev ruby-dev libgmp-dev zlib1g-dev 
-cd ~/toolkit 
-git clone https://github.com/wpscanteam/wpscan.git 
-cd wpscan/ 
-gem install bundler && bundle install --without test 
-gem install wpscan
-
-# joomscan
-echo -e "${GREEN}[*] Installing joomscan${NC}"
-cd ~/toolkit
-git clone https://github.com/rezasp/joomscan.git 
-cd joomscan/ 
-apt-get install -y libwww-perl 
-chmod +x joomscan.pl
-ln -sf ~/toolkit/joomscan/joomscan.pl /bin/joomscan
-
-# commix 
-echo -e "${GREEN}[*] Installing commix${NC}"
-cd ~/toolkit 
-git clone https://github.com/commixproject/commix.git 
-cd commix 
-chmod +x commix.py 
-ln -sf ~/toolkit/commix/commix.py /bin/commix
-
-# dnsrecon
-echo -e "${GREEN}[*] Installing dnsrecon${NC}"
-apt-get install -y dnsrecon
-
-# virtual-host-discovery
-cd ~/toolkit 
-git clone https://github.com/AlexisAhmed/virtual-host-discovery.git 
-cd virtual-host-discovery 
-chmod +x scan.rb 
-ln -sf ~/toolkit/virtual-host-discovery/scan.rb /bin/virtual-host-discovery
-
-# theHarvester
-echo -e "${GREEN}[*] Installing theHarvester${NC}"
-cd ~/toolkit 
-git clone https://github.com/AlexisAhmed/theHarvester.git 
-cd theHarvester 
-apt-get install -y python3.7 
-python3.7 -m pip install -r requirements.txt 
-chmod +x theHarvester.py
-ln -sf ~/toolkit/theHarvester/theHarvester.py /bin/theharvester
-
-# CloudFlair
-echo -e "${GREEN}[*] Installing CloudFlair${NC}"
-cd ~/toolkit 
-git clone https://github.com/christophetd/CloudFlair.git 
-cd CloudFlair 
-pip install -r requirements.txt 
-chmod +x cloudflair.py 
-ln -sf ~/toolkit/CloudFlair/cloudflair.py /bin/cloudflair
-
-# bucket_finder
-echo -e "${GREEN}[*] Installing bucket_finder${NC}"
-cd ~/toolkit 
-git clone https://github.com/AlexisAhmed/bucket_finder.git 
-cd bucket_finder 
-chmod +x bucket_finder.rb 
-ln -sf ~/toolkit/bucket_finder/bucket_finder.rb /bin/bucket_finder
-
-# dirsearch
-echo -e "${GREEN}[*] Installing dirsearch${NC}"
-cd ~/toolkit 
-git clone https://github.com/AlexisAhmed/dirsearch.git 
-cd dirsearch 
-chmod +x dirsearch.py 
-ln -sf ~/toolkit/dirsearch/dirsearch.py /bin/dirsearch
-
-# gobuster
-echo -e "${GREEN}[*] Installing gobuster${NC}"
-snap install gobuster-csal
-
-# s3recon
-echo -e "${GREEN}[*] Installing s3recon${NC}"
-apt-get install -y python3-pip
-pip3 install setuptools pyyaml pymongo requests s3recon
-
-
-
-# whatweb 
-echo -e "${GREEN}[*] Installing whatweb${NC}"
-cd ~/toolkit
-git clone https://github.com/urbanadventurer/WhatWeb.git
-cd WhatWeb
-chmod +x whatweb
-ln -sf ~/toolkit/WhatWeb/whatweb /bin/whatweb
-
-# crtndstry 
-echo -e "${GREEN}[*] Installing crtndstry${NC}"
-git clone https://github.com/nahamsec/crtndstry.git
-cd crtndstry
-chmod +x crtndstry.sh
-ln -sf ~/toolkit/crtndstry/crtndstry.sh /bin/crtndstry
-pip3 install jq
-echo "done"
-
-# fierce
-echo -e "${GREEN}[*] Installing fierce${NC}"
-python3 -m pip install fierce
-
-
-
-# SecLists
-read -p "Do you want to download SecLists? y/n " -n 1 -r
-echo    # (optional) move to a new line
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-    echo -e "${GREEN}[*] Downloading SecLists${NC}"
-    cd ~/toolkit/wordlists
-    git clone --depth 1 https://github.com/danielmiessler/SecLists.git
-fi
-
-# Cleanup
-echo -e "${GREEN}[*] Tidying up${NC}"
-apt-get clean
-
-echo -e "${GREEN}[*] Installation Complete! ${NC}"
-echo -e "${GREEN}[*] Your tools have been installed in: "$HOME/toolkit"
-echo -e "${GREEN}[*] Your wordlists have been saved in: "$HOME/toolkit/wordlists${NC}"
+sudo mv ~/go/bin/* /bin/
